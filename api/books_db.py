6618495f2a9717e6c779de9107b41daa888325ae
Martin Algañaraz <idcmardelplata@gmail.com>
2023-11-25 13:31:02 -0300

    Add some endpoints

---
+++ api/books_db.py
@@ -0,0 +1,72 @@
+books_db = [
+  {
+    "name": "Clean Code",
+    "author": "Robert C. Martin",
+    "year": "2008",
+    "Description": "A guide to writing maintainable and readable code",
+    "ISBN": "978-0132350884"
+  },
+  {
+    "name": "The Pragmatic Programmer",
+    "author": "Andrew Hunt, David Thomas",
+    "year": "1999",
+    "Description": "Your journey to mastery, featuring real-world advice from the trenches",
+    "ISBN": "978-0201616224"
+  },
+  {
+    "name": "Design Patterns",
+    "author": "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides",
+    "year": "1994",
+    "Description": "Elements of Reusable Object-Oriented Software",
+    "ISBN": "978-0201633610"
+  },
+  {
+    "name": "Introduction to Algorithms",
+    "author": "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein",
+    "year": "2009",
+    "Description": "A comprehensive introduction to the algorithms",
+    "ISBN": "978-0262033848"
+  },
+  {
+    "name": "Code Complete",
+    "author": "Steve McConnell",
+    "year": "1993",
+    "Description": "A practical handbook of software construction",
+    "ISBN": "978-0735619678"
+  },
+  {
+    "name": "Cracking the Coding Interview",
+    "author": "Gayle Laakmann McDowell",
+    "year": "2015",
+    "Description": "189 Programming Questions and Solutions",
+    "ISBN": "978-0984782857"
+  },
+  {
+    "name": "The C Programming Language",
+    "author": "Brian W. Kernighan, Dennis M. Ritchie",
+    "year": "1978",
+    "Description": "The definitive guide to C programming language",
+    "ISBN": "978-0131103627"
+  },
+  {
+    "name": "Head First Design Patterns",
+    "author": "Eric Freeman, Elisabeth Robson, Bert Bates, Kathy Sierra",
+    "year": "2004",
+    "Description": "A brain-friendly guide to understanding design patterns",
+    "ISBN": "978-0596007126"
+  },
+  {
+    "name": "Refactoring",
+    "author": "Martin Fowler",
+    "year": "1999",
+    "Description": "Improving the design of existing code",
+    "ISBN": "978-0201485677"
+  },
+  {
+    "name": "Effective Java",
+    "author": "Joshua Bloch",
+    "year": "2001",
+    "Description": "Best practices for the Java programming language",
+    "ISBN": "978-0134685991"
+  }
+]
--- api/server.py
+++ api/server.py
@@ -1,1 +1,54 @@
+from fastapi import FastAPI
+import uvicorn
+from books_db import books_db
 
+from pydantic import BaseModel
+
+class Book(BaseModel):
+    name: str
+    author: str
+    year: int
+    Description: str
+    ISBN: str
+
+app = FastAPI()
+
+
+# Comienzo del router
+
+@app.get("/api/v1/books/{book_id}")
+async def read_books(book_id):
+    """Return book id"""
+    return list(filter(lambda item: (item["ISBN"] == book_id), books_db))
+
+@app.get("/api/v1/books")
+async def read_books():
+    """Return all books"""
+    return books_db
+
+@app.post("/api/v1/books")
+async def create_book(book: Book):
+    books_db.append(book.dict())
+    return book
+
+@app.put("/api/v1/books/{book_id}")
+async def update_book(book_id: str, book: Book):
+    update_db = [ book if item["ISBN"] == book_id else item for item in books_db]
+    books_db = update_db
+    return book
+
+
+@app.path("/api/v1/books/{book_id}")
+async def update_isbn(item_id: str, new_isbn: str):
+    pass
+
+@app.delete("/api/v1/books/{book_id}")
+async def delete_book(book_id):
+    pass
+
+
+
+if __name__ == "__main__":
+    config = uvicorn.Config("server:app", port=8080, log_level="info", reload=True)
+    server = uvicorn.Server(config)
+    server.run()
