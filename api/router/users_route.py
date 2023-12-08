f94895c5ddc9c66f00a6378575b80439fb296b66
Martin Algañaraz <idcmardelplata@gmail.com>
2023-12-08 19:19:30 -0300

    basic token feature, fail loading from env vars

--- api/router/users_route.py
+++ api/router/users_route.py
@@ -1,31 +1,82 @@
 from fastapi import APIRouter
 
+from fastapi import status, HTTPException, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import RedirectResponse
+from uuid import uuid4
+from tokens.utils import (
+        get_hashed_password,
+        create_access_token,
+        create_refresh_token,
+        verify_password
+        )
+
 from pydantic import BaseModel
 from typing import Annotated
 
-class NewUser(BaseModel):
-    user_id: int
-    first_name: str
-    last_name: str
-    email: str
-    phone: str
-    birth: str
-
-
 user_route = APIRouter(prefix="/users", tags=["users"])
+
+class UserOut(BaseModel):
+    email: str
+    password: str
+    user_id: str
+
+class UserAuth(BaseModel):
+    email: str
+    password: str
+
+class TokenSchema(BaseModel):
+    access_token: str
+    refresh_token: str
+
+db: dict = {}
+
+
+@user_route.post("/signup", summary="Create new user", response_model=UserOut)
+async def create_user(data: UserAuth):
+    user = db.get(data.email, None)
+    if user is not None:
+        raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="User with this email already exists")
+
+    user = {
+            'email': data.email,
+            'password': get_hashed_password(data.password),
+            'user_id': str(uuid4())
+            }
+    print(user)
+    db[data.email] = user #guarda el usuario en la base de datos
+    print(db)
+    return user
+
+@user_route.post("/login", summary="Create access and refresh tokens for user", response_model=TokenSchema)
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = db.get(form_data.username, None)
+    if user is None: #Verifica el usuario
+        raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Incorrect email or password"
+                )
+
+    hashed_pass = user["password"]
+
+    if not verify_password(form_data.password, hashed_pass): #Verifica la contraseña
+        raise HTTPException(
+                status_code = status.HTTP_400_BAD_REQUEST,
+                detail="Incorrect email or password"
+                )
+
+    return {
+            "access_token": create_access_token(user["email"]),
+            "refresh_token": create_refresh_token(user["email"])
+            }
+
+
+
+
 
 @user_route.get("/")
 async def get_users():
     return {"msg": "Not implemented"}
 
-@user_route.post("/")
-async def add_user(user: NewUser):
-    raise NotImplemented()
-
-@user_route.put("/{user_id}")
-async def update_user(user_id: int, user: NewUser):
-    raise NotImplemented()
-
-@user_route.delete("/")
-async def delete_user(id: int):
-    raise NotImplemented()
--- api/tokens/utils.py
+++ api/tokens/utils.py
@@ -1,24 +1,23 @@
 import os
 from datetime import datetime, timedelta
 from typing import Union, Any
-from jose import jwt
+import bcrypt
+import jwt
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 # 7 days
 ALGORITHM = "HS256"
-JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-JWT_REFRESH_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+JWT_SECRET_KEY = "LLAVESUPERSECRETA"
+JWT_REFRESH_SECRET_KEY = "CARGAESTODESDEUNAVARIABLEDEENTORNO-raton"
+SALT=bcrypt.gensalt()
 
-from passlib.context import CryptContext
+def get_hashed_password(password: str):
+    password_to_bytes = password.encode('utf-8')
+    return bcrypt.hashpw(password_to_bytes, SALT)
 
-password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-# Hash and verify password
-def get_hashed_password(password: str) -> str:
-    return password_context.hash(password)
-
-def verify_password(password: str, hashed_password: str) -> bool:
-    return password_context.verify(password, hashed_password)
+def verify_password(password: str, hashed_password: bytes) -> bool:
+    password_to_bytes = password.encode('utf-8')
+    return bcrypt.checkpw(password_to_bytes, hashed_password)
 
 def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
     if expires_delta is not None:
@@ -26,8 +25,9 @@ def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> 
     else:
         expires_delta = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
 
-    to_encode = {"exp": expires_delta, "sub": str(subject)}
-    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
+    to_encode = {"exp": expires_delta, "sub": subject}
+    #FIXME: cambiar lo hardcodeado por variables de entorno y verificar que realmente se lean
+    encoded_jwt = jwt.encode(to_encode, key="hola123", algorithm="HS256")
     return encoded_jwt
 
 def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) -> str:
@@ -35,8 +35,11 @@ def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) ->
         expires_delta = datetime.utcnow() + expires_delta
     else:
         expires_delta = datetime.utcnow() + timedelta(minutes = REFRESH_TOKEN_EXPIRE_MINUTES)
-    to_encode = {"exp": expires_delta, "sub": str(subject)}
-    encoded_jwt = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, ALGORITHM)
+    to_encode = {"exp": expires_delta, "sub": subject}
+
+    #FIXME: cambiar lo hardcodeado por variables de entorno y verificar que realmente se lean
+    encoded_jwt = jwt.encode(to_encode, "hola123", "HS256")
+    # encoded_jwt = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, ALGORITHM)
     return encoded_jwt
 
 
