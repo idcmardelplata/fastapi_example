55ee62cdfedf279ad148a4a73a188dce2280c1bf
jose <jose.s.contacto@gmail.com>
2023-12-12 13:33:50 -0300

    UserMemoStorage has save method

--- servicios/memo_storage.py
+++ servicios/memo_storage.py
@@ -1,9 +1,14 @@
+from entidades.users import UserAuth
 """Modulo de gestion de almacenamiento en memoria"""
 
 class UserMemoStorage:
 
     def __init__(self):
-        self._all = {}
+        self._all = []
 
     def get_all(self) -> list[dict]:
         return self._all
+
+    def save(self,user:UserAuth):
+        if user is not None:
+            self._all.append(user)
--- tests/test_memo_storage.py
+++ tests/test_memo_storage.py
@@ -2,4 +2,16 @@ from servicios.memo_storage import UserMemoStorage
 
 def test_user_memo_storage_should_has_a_get_method_which_returns_existing_users():
     users = UserMemoStorage().get_all()
-    assert type(users) is dict
+    assert type(users) is list
+
+
+def test_user_memo_storage_should_has_a_save_method_which_stores_users():
+    storage = UserMemoStorage()
+    storage.save({
+        "email": "julian@gmail.com",
+        "password": "superpass123"
+    })
+    user = storage.get_all()[0]
+    assert user["email"] == "julian@gmail.com"
+    assert user["password"] == "superpass123"
+
