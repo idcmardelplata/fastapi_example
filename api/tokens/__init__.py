10ca901d20551e29b0ff37c9f48c8759c6428b93
Martin Algañaraz <idcmardelplata@gmail.com>
2023-12-17 17:42:41 -0300

    add tokens basic functionality

---
+++ api/tokens/__init__.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timedelta, timezone
+import jwt
+from jwt.exceptions import InvalidTokenError, ExpiredSignatureError
+import os
+from dotenv import load_dotenv
+
+REFRESH_TOKEN_EXPIRATION_TIME_DAYS = (60 * 60 * 24) * 7  #7 days
+load_dotenv()
+
+def _read_token_key_from_env():
+    return {"refresh": os.environ["REFRESH_TOKEN_KEY"], "auth": os.environ["AUTH_TOKEN_KEY"]}
+
+def create_token(user_id: str, expiration_time=0, key=None, token_type="auth") -> str:
+    current_date = datetime.now(tz=timezone.utc)
+
+    if expiration_time == 0:
+        expiration_time = current_date + timedelta(seconds=30)
+    else:
+        expiration_time = current_date + timedelta(seconds=expiration_time)
+
+    token_key = _read_token_key_from_env()[token_type] if key==None else key
+
+    payload = {"exp": expiration_time, "user_id": user_id}
+    return jwt.encode(payload, token_key)
+
+def read_token(token: str, key=None, token_type="auth" ):
+    key = _read_token_key_from_env()[token_type] if key == None else key
+    return  jwt.decode(token, key, algorithms=["HS256"])
+
+
+def create_tokens(user_id: str) -> dict[str, str]:
+    auth_token = create_token(user_id, token_type="auth")
+    refresh_token = create_token(user_id, expiration_time=REFRESH_TOKEN_EXPIRATION_TIME_DAYS, token_type="refresh")
+    return {"auth_token": auth_token, "refresh_token": refresh_token}
+
+
+
+
---
+++ tests/test_auth.py
@@ -0,0 +1,37 @@
+import os
+import pytest
+from api.tokens import create_token, read_token, create_tokens, InvalidTokenError
+
+def test_given_user_id_should_return_a_auth_token():
+    user_id = "356adf35d5bca05c96a878912025952a1c184c3c91be000ac345addbefb1660b"
+    token = create_token(user_id, expiration_time=1, key="hola123")
+    assert type(token) is str
+
+    decoded_token_info = read_token(token, key="hola123")
+    assert decoded_token_info["user_id"] == user_id
+
+def test_should_be_fail_if_keys_not_match():
+    user_id = "356adf35d5bca05c96a878912025952a1c184c3c91be000ac345addbefb1660b"
+    token = create_token(user_id, expiration_time=1, key="correct-key")
+
+    with pytest.raises(InvalidTokenError):
+        read_token(token, key="wrong-key")
+
+def test_factory_should_create_auth_and_refresh_tokens():
+    user_id = "356adf35d5bca05c96a878912025952a1c184c3c91be000ac345addbefb1660b"
+    os.environ["AUTH_TOKEN_KEY"] = "4f037dd30e2e0e92873194ddb7605f4987e68742bc0e67866b15dcded5e6daf2"
+    os.environ["REFRESH_TOKEN_KEY"] = "e35269023e3f7bb7f7b7e53175bf4dba0ed4ebccf0d7a6ee18a16fd3d43fe042"
+
+    tokens = create_tokens(user_id)
+
+    auth_token = tokens["auth_token"]
+    refresh_token = tokens["refresh_token"]
+
+    refresh_data = read_token(refresh_token, token_type="refresh")
+    auth_data = read_token(auth_token, token_type="auth")
+
+    assert refresh_data["user_id"] == user_id
+    assert auth_data["user_id"] == user_id
+
+    assert refresh_data["exp"] > auth_data["exp"]
+
