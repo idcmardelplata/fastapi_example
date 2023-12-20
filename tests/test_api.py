9b65123aa3d012e24a76c678340f02dd8b989479
Martin Algañaraz <idcmardelplata@gmail.com>
2023-12-19 23:29:53 -0300

    test: added testr for users register and loggin

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,48 +1,54 @@
-# from fastapi.testclient import TestClient
-# from api.server import app
-# import pytest
-#
-# client = TestClient(app)
-
-# @pytest.mark.integration
-# def test_should_create_a_new_user():
-#     response = client.post("/users/signin",
-#                            json={"email": "idcmardelplata@gmail.com", "password": "salvenalaspapas123"})
-#     assert response.status_code == 200
-#     assert response.json() == {"msg": "user added sucefully"}
-
-# @pytest.mark.integration
-# def test_unprotected_route_can_by_access_for_non_authenticated_users():
-#     response = client.get("/posts")
-#     assert response.status_code == 200
-#     assert type(response.json()) is list
-#     assert len(response.json()) == 100
-#
-# @pytest.mark.integration
-# def test_protected_route():
-#     # Make new user
-#     response = client.post("/users/signin",
-#                             json={"email": "idcmardelplata@gmail.com", "password": "salvenalaspapas123"})
-#     assert response.status_code == 200
-#     assert response.json() == {"msg": "user added sucefully"}
-#
-#     response = client.post("/users/login",
-#                            content={'email': 'idcmardelplata@gmail.com', 'password': 'clave123'})
-#
-#     assert type(response.json()) is dict
-#
-#     data = response.json()
-#     assert data == {"msg": "Incorrect email or password"}
-#     # auth_token =  data["msg"]["Loggin sucess"]["auth_token"]
-#     # refresh_token = data["msg"]["Loggin sucess"]["refresh_token"] 
-#
-#     # assert auth_token != refresh_token
+from fastapi.testclient import TestClient
+from api.server import app
+import pytest
 
 
+@pytest.mark.integration
+def test_should_create_a_new_user():
+    client =  TestClient(app)
+    user_data = {
+            "email": "idcmardelplata@gmail.com",
+            "password": "salvenalaspapas123"
+            }
+    response = client.post("/users/signin",
+                            json=user_data)
+    assert response.status_code == 200, response.text
+    assert response.json() == {"msg": "user added sucefully"}
 
+@pytest.mark.integration
+def test_create_new_user_should_fail_with_empty_password():
+    client =  TestClient(app)
+    user_data = {
+            "email": "idcmardelplata@gmail.com",
+            "password": ""
+            }
+    response = client.post("/users/signin",
+                            json=user_data)
+    assert response.status_code == 200, response.text
+    assert response.json() == {"msg": f"error on register {user_data['email']}"}
 
+@pytest.mark.integration
+def test_login():
+    client =  TestClient(app)
+    user_data = {
+            "email": "idcmardelplata@gmail.com",
+            "password": "salvenalaspapas123"}
 
+    response = client.post("/users/signin", json=user_data)
 
+    assert response.status_code == 200, response.text
 
+    login = {
+            "username": "idcmardelplata@gmail.com",
+            "password": "salvenalaspapas123"}
 
-    # response = client.get("/comments", headers={'Autorization: Bearer': auth_token})
+    response = client.post("/users/login",
+                           data=login,
+                           headers={"Content-Type": "application/x-www-form-urlencoded"},
+                           allow_redirects=True)
+
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert type(data["auth_token"]) is str
+    assert type(data["refresh_token"]) is str
+
